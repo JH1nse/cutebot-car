d70de93cf033e6917016caff72019fda9ff4b3ee
JH1nse <jairohinse@kpnmail.nl>
2025-08-26 15:05:46 +0200

    eindproduct car

--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ enum RadioMessage {
     Checkpoint2 = 32327,
     Checkpoint3 = 63779,
     Checkpoint4 = 53120,
+    Checkpoint1Behaald = 56413,
+    Checkpoint2Behaald = 24361,
+    Checkpoint3Behaald = 11045,
+    Checkpoint4Behaald = 14638,
     StartTijd = 340,
     Finish = 5694,
     rechtdoor = 12848,
@@ -15,11 +19,26 @@ enum RadioMessage {
     rem = 58635
 }
 radio.onReceivedMessage(RadioMessage.rechtdoor, function () {
-	
+    funny_honk = randint(1, 10)
+    if (funny_honk == 10) {
+        music.play(music.stringPlayable("B A G - G - G A ", 400), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("- C5 - C5 - C5 - B ", 400), music.PlaybackMode.UntilDone)
+    } else {
+        music.play(music.stringPlayable("B - B B B - - - ", 300), music.PlaybackMode.UntilDone)
+    }
+})
+radio.onReceivedMessage(RadioMessage.Finish, function () {
+    radio.sendMessage(RadioMessage.Checkpoint3Behaald)
+})
+radio.onReceivedMessage(RadioMessage.Checkpoint1, function () {
+    radio.sendMessage(RadioMessage.Checkpoint1Behaald)
 })
 radio.onReceivedMessage(RadioMessage.rechts, function () {
     CutebotPro.pwmCruiseControl(100, 20)
-    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.West)
+})
+radio.onReceivedMessage(RadioMessage.Checkpoint2, function () {
+    radio.sendMessage(RadioMessage.Checkpoint2Behaald)
 })
 radio.onReceivedMessage(RadioMessage.rem, function () {
     CutebotPro.pwmCruiseControl(0, 0)
@@ -37,12 +56,13 @@ radio.onReceivedMessage(RadioMessage.vooruit, function () {
 })
 radio.onReceivedMessage(RadioMessage.links, function () {
     CutebotPro.pwmCruiseControl(20, 100)
-    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.East)
 })
 radio.onReceivedMessage(RadioMessage.achteruit, function () {
     CutebotPro.fullAstern()
     basic.showArrow(ArrowNames.South)
 })
+let funny_honk = 0
 radio.setTransmitPower(7)
 radio.setGroup(35)
 CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffffff)
