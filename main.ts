0056ee7959911bffda9dbb00b665ffa5b1ea9f93
JH1nse <jairohinse@kpnmail.nl>
2025-08-26 10:07:16 +0200

    ja

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
 enum RadioMessage {
+    Checkpoint1 = 25201,
+    Checkpoint2 = 32327,
+    Checkpoint3 = 63779,
+    Checkpoint4 = 53120,
+    StartTijd = 340,
+    Finish = 5694,
     rechtdoor = 12848,
     links = 30556,
     rechts = 39515,
     achteruit = 43484,
     vooruit = 44692,
-    message1 = 49434
+    message1 = 49434,
+    Start = 56380,
+    rem = 58635
 }
 radio.onReceivedMessage(RadioMessage.rechtdoor, function () {
-    music.play(music.stringPlayable("B - B B B - - - ", 300), music.PlaybackMode.UntilDone)
+	
 })
 radio.onReceivedMessage(RadioMessage.rechts, function () {
     CutebotPro.pwmCruiseControl(100, 20)
     basic.showArrow(ArrowNames.East)
+})
+radio.onReceivedMessage(RadioMessage.rem, function () {
+    CutebotPro.pwmCruiseControl(0, 0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
 })
 radio.onReceivedMessage(RadioMessage.vooruit, function () {
     CutebotPro.fullSpeedAhead()
@@ -25,15 +43,6 @@ radio.onReceivedMessage(RadioMessage.achteruit, function () {
     CutebotPro.fullAstern()
     basic.showArrow(ArrowNames.South)
 })
-radio.onReceivedMessage(RadioMessage.message1, function () {
-    CutebotPro.pwmCruiseControl(0, 0)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # # # .
-        . . . . .
-        . . . . .
-        `)
-})
+radio.setTransmitPower(7)
 radio.setGroup(35)
 CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffffff)
